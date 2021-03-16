f12b0cc2aba8e9974307a2ff2b6bb46f01e77994
jgaman <gaman2404@gmail.com>
2021-03-16 11:41:01 +0100

    chapiter 2

--- python_fundamentals-master/python_fundamentals-master/02_basic_datatypes/1_numbers/02_01_cylinder.py
+++ python_fundamentals-master/python_fundamentals-master/02_basic_datatypes/1_numbers/02_01_cylinder.py
@@ -4,3 +4,9 @@ of a cylinder with a radius of 3.14 and a height of 5. Print out the result.
 
 
 '''
+#Volume = πr2h
+volume = (3.14 * 3.14 * 2 * 5)
+# surface = 2πrh+2πr2
+surface = (2 * 3.14 * 3.14 * 5) + (2 * 3.14 * 3.14 * 2)
+print("volume =", volume)
+print("surface =", surface)
