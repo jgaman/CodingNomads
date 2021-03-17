5414c4eac91e44855eada80dabad798509d93d16
jgaman <gaman2404@gmail.com>
2021-03-17 12:54:03 +0100

    section 2 exo 4

--- python_fundamentals-master/python_fundamentals-master/02_basic_datatypes/1_numbers/02_04_temp.py
+++ python_fundamentals-master/python_fundamentals-master/02_basic_datatypes/1_numbers/02_04_temp.py
@@ -10,3 +10,6 @@ Output should read like - "81.32 degrees fahrenheit = 27.4 degrees celsius"
 
 
 '''
+Fahrenheit = (input("Enter your Fahrenheit degree there: "))
+celsius = (int(Fahrenheit) - 32) * (5 / 9)
+print(Fahrenheit, "degrees fahrenheit =", celsius, "degrees celsius")
