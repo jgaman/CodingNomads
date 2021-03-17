1f9c699ad82b5b1a2eadfaed472240d636053965
jgaman <gaman2404@gmail.com>
2021-03-17 12:33:32 +0100

    section

--- python_fundamentals-master/python_fundamentals-master/02_basic_datatypes/1_numbers/02_03_runner.py
+++ python_fundamentals-master/python_fundamentals-master/02_basic_datatypes/1_numbers/02_03_runner.py
@@ -4,4 +4,7 @@ If a runner runs 10 miles in 30 minutes and 30 seconds,
 What is his/her average speed in kilometers per hour? (Tip: 1 mile = 1.6 km)
 
 '''
+#speed in km per hour = (Distance) / (time * 24)
+speed = 16/(0.5 * 24)
+print(speed, "Km/H")
 
