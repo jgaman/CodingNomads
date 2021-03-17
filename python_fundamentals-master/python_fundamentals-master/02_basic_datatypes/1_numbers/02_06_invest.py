8acf4d0642609b9601f63d29954cb0dc8ac4d6ff
jgaman <gaman2404@gmail.com>
2021-03-17 15:18:16 +0100

    section 2 exo 11

--- python_fundamentals-master/python_fundamentals-master/02_basic_datatypes/1_numbers/02_06_invest.py
+++ python_fundamentals-master/python_fundamentals-master/02_basic_datatypes/1_numbers/02_06_invest.py
@@ -7,3 +7,10 @@ Take in the following three values from the user:
 Print the future values to the console.
 
 '''
+
+investment_amount = int(input("Enter your investment amount:"))
+interest_rate_in_percentage = float(input("Enter your interest rate in percentage:"))
+number_of_years_to_invest = int(input("Enter the number of years to invest:"))
+# FV=I×(1+(R×T))
+future_values = (investment_amount * (1 + (interest_rate_in_percentage * number_of_years_to_invest)))
+print("Your future values are:", round(future_values, 2))
