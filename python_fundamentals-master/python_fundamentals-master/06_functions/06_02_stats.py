075bbeae6bb80b6b872f6dc845b20651a01192fb
jgaman <gaman2404@gmail.com>
2021-04-05 16:22:33 +0200

    section 6 exo 2

--- python_fundamentals-master/python_fundamentals-master/06_functions/06_02_stats.py
+++ python_fundamentals-master/python_fundamentals-master/06_functions/06_02_stats.py
@@ -6,8 +6,12 @@ Print these values to the console when calling the function.
 
 example_list = [1, 2, 3, 4, 5, 6, 7]
 
-def stats():
-  # define the function here
-  pass
+
+def stats(list):
+  return max(list)
+
 
 # call the function below here
+stats(example_list)
+
+
