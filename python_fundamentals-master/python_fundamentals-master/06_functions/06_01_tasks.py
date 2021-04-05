8ffc44cb68dcc487bb86e024bc32580cebc0929b
jgaman <gaman2404@gmail.com>
2021-04-05 13:08:07 +0200

    section 2 exo 7

--- python_fundamentals-master/python_fundamentals-master/06_functions/06_01_tasks.py
+++ python_fundamentals-master/python_fundamentals-master/06_functions/06_01_tasks.py
@@ -1,10 +1,19 @@
-'''
+"""
 
 Write a script that completes the following tasks.
 
-'''
+"""
 
 # define a function that determines whether the number is divisible by 4 or 7 and returns a boolean
+user_input = int(input("Enter your number there:"))
+def num_divisible_by_4_7 (user_input):
+    if user_input % 4 == 0:
+        return f"{user_input} is divisible by 4"
+    elif user_input % 7 == 0:
+        return f"{user_input} is divisible by 7"
+    else:
+        return "Your number is not divisible by 4 or 7"
+
 
 # define a function that determines whether a number is divisible by both 4 and 7 and returns a boolean
 
