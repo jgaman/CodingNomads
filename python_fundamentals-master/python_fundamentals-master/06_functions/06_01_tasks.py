d3ee6ef6f0638fe5816a76267bfdec7b4cb93d34
jgaman <gaman2404@gmail.com>
2021-06-25 11:13:04 +0200

    correction

--- python_fundamentals-master/python_fundamentals-master/06_functions/06_01_tasks.py
+++ python_fundamentals-master/python_fundamentals-master/06_functions/06_01_tasks.py
@@ -1,14 +1,3 @@
-"""
-
-Write a script that completes the following tasks.
-
-"""
-
-# define a function that determines whether the number is divisible by 4 or 7 and returns a boolean
-
-user_input = input("Enter your number there to see if it divisible by 4 and 7: ")
-int_user_num = int(user_input)
-
 
 def div_by_4_7(num):
     print("It's", num % 4 == 0 and num % 7 == 0, "that", user_input, "is divisible by 4 and 7")
